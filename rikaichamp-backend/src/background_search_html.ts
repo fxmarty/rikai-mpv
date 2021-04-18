46c2c990c5c243bd9d2ce47c57af4947e52de91b
FxMarty <felix.marty@mines-paristech.fr>
2021-04-18 22:52:52 +0200

    add license headers back

--- rikaichamp-backend/src/background_search_html.ts
+++ rikaichamp-backend/src/background_search_html.ts
@@ -1,3 +1,40 @@
+/*
+  rikai-mpv
+  by fxmarty
+  https://github.com/fxmarty/rikai-mpv
+  ---
+  Originally based on Rikaichamp
+  by Brian Birtles
+  https://github.com/birtles/rikaichamp
+  ---
+  Originally based on Rikaikun
+  by Erek Speed
+  http://code.google.com/p/rikaikun/
+  ---
+  Originally based on Rikaichan 1.07
+  by Jonathan Zarate
+  http://www.polarcloud.com/
+  ---
+  Originally based on RikaiXUL 0.4 by Todd Rudick
+  http://www.rikai.com/
+  http://rikaixul.mozdev.org/
+  ---
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  ---
+  Please do not change or remove any of the copyrights or links to web pages
+  when modifying any of the files. - Jon
+*/
+
 import { WordsDictionary } from './data_words.js';
 import {NamesDictionary} from './data_names.js';
 import { searchNames } from './jpdict.js'
