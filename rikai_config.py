8eb90ffa2e1ec3efe92675a6c7f548541404a27f
FxMarty <9808326+fxmarty@users.noreply.github.com>
2021-05-21 18:40:49 +0200

    reset default options

--- rikai_config.py
+++ rikai_config.py
@@ -14,13 +14,13 @@ style_subs = '''
 default_font_point_size = 50
 
 # default zoom of the popup
-default_zoom_popup = 1.4
+default_zoom_popup = 1
 
 # number of pixels to leave at the bottom of the screen, below subtitles
 bottom_spacing_pixels = 100
 
 # indicates the index of the screen the subtitles will be displayed on, to set manually
-n_screen = 1
+n_screen = 0
 
 # Ideally, more parameters could be added here, as interSubs does:
 # https://github.com/oltodosel/interSubs/blob/master/interSubs_config.py
